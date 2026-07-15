397bdba469158ea49a7aa09a53e2d8d232bacec9
humanauction <t.a.harris@gmail.com>
2026-07-15 10:44:27 +0100

    Fix: ha_proxy/router.ts: callProvider path

--- ha_proxy/router.ts
+++ ha_proxy/router.ts
@@ -1,6 +1,6 @@
 import type { Request, Response } from "express";
 import type { SMAGEMessage, SMAGEOptions } from "../ha_core/index.js";
-import { callProvider } from "../ha_core/call/providers";
+import { callProvider } from "../ha_core/call/providers/index.js";
 import { reversibleLog } from "../ha_core/cache/log.js";
 import { applyCCR } from "../ha_core/transform/ccr.js";
 import { mapProviderRole } from "../ha_core/call/providers/roles.js";
